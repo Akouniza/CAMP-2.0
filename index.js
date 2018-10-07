a95178fbc8bd22fc52f96697a5d2ffc34d5f647b
AlexejheroYTB <alexejhero@gmail.com>
2018-10-07 20:09:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,25 @@
+/* eslint
+  prefer-destructuring: 0,
+  no-console: 0,
+*/
+
+/* tslint:disable */
+
 const Discord = require('discord.js');
+const token = require('./token.json').token;
+
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!');
+client.on('message', (message) => {
+  if (message.author.bot) return;
+
+  if (message.content === 'ping') {
+    message.reply('Pong!');
   }
 });
 
-// client.login('token');
+client.login(token);
