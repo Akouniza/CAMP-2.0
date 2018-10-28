124cd99aeaaa244f0d4e39714736875d2e93b384
AlexejheroYTB <alexejhero@gmail.com>
2018-10-28 19:41:59 +0200

    Update commands/nexus.js

---
+++ commands/nexus.js
@@ -0,0 +1,35 @@
+const Discord = require('discord.js');
+
+/**
+ * @param {Discord.GuildMember} bot
+ * @param {Discord.Client} client
+ * @param {Discord.Message} message
+ * @param {string} command
+ * @param {string[]} args
+ */
+module.exports.run = async (bot, client, config, message, command, args) => {
+  const embed = new Discord.MessageEmbed()
+    .setAuthor(bot.nickname !== undefined ? bot.nickname : bot.user.username, client.user.avatarURL())
+    .setFooter(`${message.member.nickname !== undefined ? message.member.nickname : message.member.user.username}: ${config.prefix}${command} ${args.join(' ')}`, message.member.user.avatarURL());
+
+  if (args.length === 1 && String(Number(args[0])) === args[0]) {
+    require('../util/cors.js')({
+      method: 'GET',
+      url: `nexusmods.com/subnautica/mods/${args[0]}`,
+      data: '',
+    }, (data) => {
+      if (data === null || data === '') return message.channel.send(embed.setColor('RED').setDescription('Could not get data from NexusMods!')) && client.error(`Could not get data from NexusMods! Mod ID = ${args[0]}`);
+      const parsedData = String(data.replace(/<script[^>]*>[^]*?<\/script>/gim, ''));
+      message.channel.send(embed.setDescription(parsedData));
+    });
+  }
+};
+
+module.exports.help = {
+  name: 'nexus',
+  description: 'Retrieves mod information from nexusmods.com',
+  usage: '<id>/<name>',
+  examples: ['113', 'SMLHelper V2'],
+  aliases: null,
+  permission: 'user',
+};
