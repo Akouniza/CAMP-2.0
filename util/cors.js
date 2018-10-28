bcdf0c33abc5796e93d5484d4d8192804a4d6fbf
AlexejheroYTB <alexejhero@gmail.com>
2018-10-28 19:41:34 +0200

    Update util/cors.js

---
+++ util/cors.js
@@ -0,0 +1,23 @@
+const XMLHttpRequest = require('xmlhttprequest').XMLHttpRequest;
+
+const corsAnywhere = 'https://cors-anywhere.herokuapp.com/';
+
+/**
+ * Do a CORS request to corsAnywhere and get receive the response
+ * @param {function} printResult
+ */
+module.exports = function doCORSRequest(options, printResult) {
+  var x = new XMLHttpRequest();
+  x.open(options.method, corsAnywhere + options.url);
+  x.onload = x.onerror = function () {
+    printResult(
+      options.method + ' ' + options.url + '\n'
+      + x.status + ' ' + x.statusText + '\n\n'
+      + (x.responseText || ''),
+    );
+  };
+  if (/^POST/i.test(options.method)) {
+    x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+  }
+  x.send(options.data);
+};
