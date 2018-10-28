de0ed381b8c3c830d861b2c533e8b892ee3c7fb5
AlexejheroYTB <alexejhero@gmail.com>
2018-10-28 19:14:37 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const ms = require('ms');
-const fs = require('fs');
 const token = require('./token.json').token;
 const config = require('./config.json');
 
@@ -9,52 +8,11 @@ const client = new Discord.Client();
 client.timeout = { time: 0, channel: null };
 client.commands = new Discord.Collection();
 
-client.debug = (message) => {
-  client.guilds.get(config.guildID).channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setDescription(message).setAuthor('Debug'));
-  client.debug(message);
-};
+client.on('ready', async () => {
+  await require('./events/ready/logging.js')(client, config);
+  await require('./events/ready/loadcommands.js')(client);
+  await require('./events/ready/setpresence.js')(client, config);
 
-client.log = (message) => {
-  client.guilds.get(config.guildID).channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setDescription(message).setAuthor('Log').setColor('BLUE'));
-  client.log(message);
-};
-
-client.warn = (message) => {
-  client.guilds.get(config.guildID).channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setDescription(message).setAuthor('Warn').setColor('ORANGE'));
-  client.warn(message);
-};
-
-client.error = (message) => {
-  client.guilds.get(config.guildID).channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setDescription(message).setAuthor('Error').setColor('RED'));
-  client.warn(message);
-};
-
-fs.readdir('./commands/', (err, files) => {
-  if (err) client.error(err);
-  const jsfiles = files.filter(f => f.split('.').pop() === 'js');
-
-  if (jsfiles.length <= 0) return client.warn('There are no commands to load...');
-
-  client.debug(`Loading ${jsfiles.length} commands...`);
-
-  jsfiles.forEach((f) => {
-    const props = require(`./commands/${f}`);
-    client.debug(`Loading command '${props.help.name}'...`);
-    client.commands.set(props.help.name, props);
-  });
-});
-
-client.on('ready', () => {
-  const presence = {
-    status: 'online',
-    afk: false,
-    activity: {
-      name: `${config.prefix}help`,
-      type: 'PLAYING',
-      url: 'https://github.com/AlexejheroYTB/CAMP-2.0',
-    },
-  };
-  client.user.setPresence(presence);
   client.log(`Logged in as '${client.user.tag}'`);
 });
 
