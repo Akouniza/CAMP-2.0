49706e04344057897c6c799607fd6f024e61940c
AlexejheroYTB <alexejhero@gmail.com>
2018-11-11 18:32:27 +0200

    Changed the about command to mention me

--- commands/about.js
+++ commands/about.js
@@ -14,7 +14,7 @@ module.exports.run = async (bot, client, config, message, command, args) => {
       .setAuthor(bot.nickname ? bot.nickname : bot.user.username, client.user.avatarURL())
       .setFooter(`${message.member.nickname ? message.member.nickname : message.member.user.username}: ${config.prefix}${command} ${args.join(' ')}`, message.member.user.avatarURL())
       .setDescription('CAMP 2.0, the successor of CAMP, is a bot made for the Subnautica Modding discord server.')
-      .addField('Author', message.guild.members.get(config.devID).user.tag, true)
+      .addField('Author', `<@${message.guild.members.get(config.devID).user.id}>`, true)
       .addField('Version', changelog.version, true)
       .addField('Changes', '• ' + changelog.items.join('\n• '));
     message.channel.send(embed).catch(console.error);
