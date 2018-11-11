e37c40a790f23fb937a37d7b4152259b804667c9
AlexejheroYTB <alexejhero@gmail.com>
2018-11-11 18:28:55 +0200

    logging changes

--- events/ready/logging.js
+++ events/ready/logging.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 const Discord = require('discord.js');
 
 /**
@@ -5,56 +6,23 @@ const Discord = require('discord.js');
  */
 module.exports = async (client, config) => {
   const temp = [];
-  const cons = console;
   console.debug = (info, params) => {
-    if (temp.includes(info) || info.toLowerCase().includes('heartbeat')) {
-      temp.splice(temp.indexOf(info), 1);
-    } else {
-      temp.push(info);
-      if (params) cons.debug(info, ...params);
-      else cons.debug(info);
-      client.emit('debug', info);
-    }
+    client.emit('debug', info);
   };
   console.log = (info, params) => {
-    if (temp.includes(info) || info.toLowerCase().includes('heartbeat')) {
-      temp.splice(temp.indexOf(info), 1);
-    } else {
-      temp.push(info);
-      if (params) cons.log(info, ...params);
-      else cons.log(info);
-      client.emit('info', info);
-    }
+    client.emit('info', info);
   };
   console.info = (info, params) => {
-    if (temp.includes(info) || info.toLowerCase().includes('heartbeat')) {
-      temp.splice(temp.indexOf(info), 1);
-    } else {
-      temp.push(info);
-      if (params) cons.log(info, ...params);
-      else cons.log(info);
-      client.emit('info', info);
-    }
+    client.emit('info', info);
   };
   console.warn = (info, params) => {
-    if (temp.includes(info) || info.toLowerCase().includes('heartbeat')) {
-      temp.splice(temp.indexOf(info), 1);
-    } else {
-      temp.push(info);
-      if (params) cons.log(info, ...params);
-      else cons.log(info);
-      client.emit('warn', info);
-    }
+    client.emit('warn', info);
   };
   console.error = (info, params) => {
-    if (temp.includes(info) || info.toLowerCase().includes('heartbeat')) {
-      temp.splice(temp.indexOf(info), 1);
-    } else {
-      temp.push(info);
-      if (params) cons.log(info, ...params);
-      else cons.log(info);
-      client.emit('error', info);
-    }
+    client.emit('error', info);
+  };
+  console.exception = (info, params) => {
+    client.emit('error', info);
   };
 
   client.debug = message => console.debug(message);
@@ -62,17 +30,38 @@ module.exports = async (client, config) => {
   client.info = message => console.info(message);
   client.warn = message => console.warn(message);
   client.error = message => console.error(message);
+  client.exception = message => console.error(message);
 
   client.on('debug', (s) => {
-    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Debug').setDescription(s));
+    if (temp.includes(s)) {
+      temp.splice(temp.indexOf(s), 1);
+    } else {
+      temp.push(s);
+      client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Debug').setDescription(s));
+    }
   });
   client.on('info', (s) => {
-    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Log').setDescription(s).setColor('BLUE'));
+    if (temp.includes(s)) {
+      temp.splice(temp.indexOf(s), 1);
+    } else {
+      temp.push(s);
+      client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Log').setDescription(s).setColor('BLUE'));
+    }
   });
   client.on('warn', (s) => {
-    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Warn').setDescription(s).setColor('ORANGE'));
+    if (temp.includes(s)) {
+      temp.splice(temp.indexOf(s), 1);
+    } else {
+      temp.push(s);
+      client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Warn').setDescription(s).setColor('ORANGE'));
+    }
   });
   client.on('error', (s) => {
-    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Error').setDescription(s).setColor('RED'));
+    if (temp.includes(s)) {
+      temp.splice(temp.indexOf(s), 1);
+    } else {
+      temp.push(s);
+      client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Error').setDescription(s).setColor('RED'));
+    }
   });
 };
