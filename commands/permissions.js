746c823911db8f4e602e7818c6f48e3b40fe2aaf
AlexejheroYTB <alexejhero@gmail.com>
2018-11-11 18:29:11 +0200

    added `!permissions`

---
+++ commands/permissions.js
@@ -0,0 +1,36 @@
+const Discord = require('discord.js');
+
+/**
+ * @param {Discord.GuildMember} bot
+ * @param {Discord.Client} client
+ * @param {Discord.Message} message
+ * @param {string} command
+ * @param {string[]} args
+ */
+module.exports.run = async (bot, client, config, message, command, args) => {
+  try {
+    const embed = new Discord.MessageEmbed()
+      .setAuthor(bot.nickname ? bot.nickname : bot.user.username, client.user.avatarURL())
+      .setFooter(`${message.member.nickname ? message.member.nickname : message.member.user.username}: ${config.prefix}${command} ${args.join(' ')}`, message.member.user.avatarURL());
+
+    const permissions = ['user'];
+    if (message.member.id === config.devID) permissions.push('trusted', 'staff', 'owner', 'dev');
+    else if (Array.from(message.member.roles.values()).includes(message.member.guild.roles.get(config.ownerRoleID))) permissions.push('trusted', 'staff', 'owner');
+    else if (Array.from(message.member.roles.values()).includes(message.member.guild.roles.get(config.staffRoleID))) permissions.push('trusted', 'staff');
+    else if (Array.from(message.member.roles.values()).includes(message.member.guild.roles.get(config.trustedRoleID))) permissions.push('trusted');
+
+    message.channel.send(embed.addField('Your permissions', permissions.join(', '))).catch(console.error);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports.help = {
+  name: 'permissions',
+  description: 'Get your permission level',
+  usage: null,
+  examples: null,
+  aliases: null,
+  permission: 'user',
+  disabled: false,
+};
