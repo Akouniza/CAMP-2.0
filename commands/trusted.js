14cd9e622f35d402920e413ce00ea6abeae0e2ed
AlexejheroYTB <alexejhero@gmail.com>
2018-11-14 14:54:05 +0000

    Update trusted.js

--- commands/trusted.js
+++ commands/trusted.js
@@ -13,7 +13,7 @@ module.exports.run = async (bot, client, config, message, command, args) => {
       .setAuthor(bot.nickname ? bot.nickname : bot.user.username, client.user.avatarURL())
       .setFooter(`${message.member.nickname ? message.member.nickname : message.member.user.username}: ${config.prefix}${command} ${args.join(' ')}`, message.member.user.avatarURL());
     if (message.mentions.members.size !== 0) return message.channel.send(embed.setColor('RED').setDescription('Invalid command usage.\nYou must mention exactly one user.')).catch(console.error);
-    if (message.mentions.members.first().id === message.member.user.id) return message.channel.send(embed.setColor('RED').setDescription('Invalid command usage.\nYou cannot use this command on yourself.')).catch(console.error);
+    if (message.mentions.members.first().user.id === message.member.user.id) return message.channel.send(embed.setColor('RED').setDescription('Invalid command usage.\nYou cannot use this command on yourself.')).catch(console.error);
     if (!message.mentions.members.first().roles.has(config.trustedRoleID)) {
       message.mentions.members.first().roles.add(config.trustedRoleID, `${message.author.tag}: ${config.prefix}${command} ${args.join(' ')}`).catch(console.error);
       embed.setDescription(`Gave the trusted role to ${message.mentions.members.first().user.tag}`);
