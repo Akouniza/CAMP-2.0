544586010d3fd5f1b2a585d12e003e261f90f149
AlexejheroYTB <alexejhero@gmail.com>
2018-11-14 15:07:06 +0000

    Update logging.js

--- events/ready/logging.js
+++ events/ready/logging.js
@@ -5,24 +5,36 @@ const Discord = require('discord.js');
  * @param {Discord.Client} client
  */
 module.exports = async (client, config) => {
-  const temp = [];
+  const debug = console.debug;
+  const log = console.log;
+  const info = console.info;
+  const warn = console.warn;
+  const error = console.error;
+  const exception = console.exception;
+
   console.debug = (info, params) => {
     client.emit('debug', info);
+    debug(info, params);
   };
   console.log = (info, params) => {
     client.emit('info', info);
+    log(info, params);
   };
   console.info = (info, params) => {
     client.emit('info', info);
+    info(info, params);
   };
   console.warn = (info, params) => {
     client.emit('warn', info);
+    warn(info, params);
   };
   console.error = (info, params) => {
     client.emit('error', info);
+    error(info, params);
   };
   console.exception = (info, params) => {
     client.emit('error', info);
+    exception(info, params);
   };
 
   client.debug = message => console.debug(message);
@@ -32,36 +44,16 @@ module.exports = async (client, config) => {
   client.error = message => console.error(message);
   client.exception = message => console.error(message);
 
-  client.on('debug', (s) => {
-    if (temp.includes(s)) {
-      temp.splice(temp.indexOf(s), 1);
-    } else {
-      temp.push(s);
-      client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Debug').setDescription(s));
-    }
+  client.on('debug', (s) =>
+    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Debug').setDescription(s));
   });
   client.on('info', (s) => {
-    if (temp.includes(s)) {
-      temp.splice(temp.indexOf(s), 1);
-    } else {
-      temp.push(s);
-      client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Log').setDescription(s).setColor('BLUE'));
-    }
+    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Log').setDescription(s).setColor('BLUE'));
   });
   client.on('warn', (s) => {
-    if (temp.includes(s)) {
-      temp.splice(temp.indexOf(s), 1);
-    } else {
-      temp.push(s);
-      client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Warn').setDescription(s).setColor('ORANGE'));
-    }
+    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Warn').setDescription(s).setColor('ORANGE'));
   });
   client.on('error', (s) => {
-    if (temp.includes(s)) {
-      temp.splice(temp.indexOf(s), 1);
-    } else {
-      temp.push(s);
-      client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Error').setDescription(s).setColor('RED'));
-    }
+    client.channels.get(config.consoleChannelID).send(new Discord.MessageEmbed().setAuthor('Error').setDescription(s).setColor('RED'));
   });
 };
