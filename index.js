7a525c11083bbd4763a61e52141ddc0cadf2193f
AlexejheroYTB <alexejhero@gmail.com>
2018-11-18 16:07:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -41,9 +41,9 @@ try {
       const logembed = new Discord.MessageEmbed()
         .setColor('RED')
         .setAuthor(bot.nickname ? bot.nickname : bot.user.username, client.user.avatarURL())
-        .addField('ERROR! The bot does not have administrator permissions!', 'The bot requires administrator permissions to ensure that it can respond to commands in any channel and have all of the required permissions (they are not checked for)')
+        .addField('ERROR! The bot does not have administrator permissions!', 'The bot requires administrator permissions to ensure that it can respond to commands in any channel and have all of the required permissions (they are not individually checked)')
         .setFooter(`${message.member.nickname ? message.member.nickname : message.member.user.username}: ${config.prefix}${command} ${args.join(' ')}`, message.member.user.avatarURL());
-      message.guild.channels.get(config.logChannelID).send(logembed);
+      message.guild.channels.get(config.consoleChannelID).send(logembed);
       embed.addField('An error occured!', 'Please contact an admin to check the log').setColor('RED');
       message.channel.send(embed);
       return;
