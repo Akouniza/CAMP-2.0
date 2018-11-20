a0eef2b5df340a66bfaf27956fe314dcf8b74525
AlexejheroYTB <alexejhero@gmail.com>
2018-11-20 09:43:04 +0200

    Create hastts.js

---
+++ commands/hastts.js
@@ -0,0 +1,30 @@
+const Discord = require('discord.js');
+
+/**
+ * @param {Discord.GuildMember} bot
+ * @param {Discord.Client} client
+ * @param {Discord.Message} message
+ * @param {string} command
+ * @param {string[]} args
+ */
+module.exports.run = async (bot, client, config, message, command, args) => {
+  try {
+    const embed = new Discord.MessageEmbed()
+      .setAuthor(bot.nickname ? bot.nickname : bot.user.username, client.user.avatarURL())
+      .setFooter(`${message.member.nickname ? message.member.nickname : message.member.user.username}: ${config.prefix}${command} ${args.join(' ')}`, message.member.user.avatarURL())
+      .setDescription(`The bot ${bot.hasPermission('SEND_TTS_MESSAGES') ? 'has' : 'doesn\'t have'} tts permissions.`);
+    message.channel.send(embed).catch(console.error);
+  } catch (e) {
+    console.error(e);
+  }
+};
+
+module.exports.help = {
+  name: 'hastts',
+  description: 'Checks if the bot has text to speech permissions',
+  usage: ' ',
+  examples: null,
+  aliases: null,
+  permission: 'dev',
+  disabled: false,
+};
