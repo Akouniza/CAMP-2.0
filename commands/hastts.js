304c2b35a3ba3dd5841540c806f0c54b67586dda
AlexejheroYTB <alexejhero@gmail.com>
2018-11-20 09:48:32 +0200

    Update hastts.js

--- commands/hastts.js
+++ commands/hastts.js
@@ -11,8 +11,13 @@ module.exports.run = async (bot, client, config, message, command, args) => {
   try {
     const embed = new Discord.MessageEmbed()
       .setAuthor(bot.nickname ? bot.nickname : bot.user.username, client.user.avatarURL())
-      .setFooter(`${message.member.nickname ? message.member.nickname : message.member.user.username}: ${config.prefix}${command} ${args.join(' ')}`, message.member.user.avatarURL())
-      .setDescription(`The bot ${bot.hasPermission('SEND_TTS_MESSAGES') ? 'has' : 'doesn\'t have'} tts permissions.`);
+      .setFooter(`${message.member.nickname ? message.member.nickname : message.member.user.username}: ${config.prefix}${command} ${args.join(' ')}`, message.member.user.avatarURL());
+    if (bot.hasPermission('SEND_TTS_MESSAGES'))
+      embed.setDescription('The bot has tts permissions')
+        .setColor('GREEN');
+    else
+      embed.setDescription('The bot doesn\'t have tts permissions')
+        .setColor('RED');
     message.channel.send(embed).catch(console.error);
   } catch (e) {
     console.error(e);
