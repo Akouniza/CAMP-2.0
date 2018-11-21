3aae7f22da6008144051b3bbb164d39b0c05171a
AlexejheroYTB <alexejhero@gmail.com>
2018-11-21 17:59:00 +0200

    Update sudo.js

--- commands/sudo.js
+++ commands/sudo.js
@@ -22,11 +22,11 @@ module.exports.run = async (bot, client, config, message, command, args) => {
     if (message.mentions.members.first().user.bot) return message.channel.send(embed.setColor('RED').setDescription('Invalid command usage.\nYou cannot sudo a bot.')).catch(console.error);
     
     message.author = message.mentions.users.first();
-    message.content = `${config.prefix}{newcommand} {newargs.join(' ')}`;
+    message.content = `${config.prefix}${newcommand} ${newargs.join(' ')}`;
     message.mentions.members.delete(Array.from(message.mentions.members.values())[0]);
     message.mentions.users.delete(Array.from(message.mentions.users.values())[0]);
     
-    console.debug(`Running command \`${message.content}\` as ${message.member.id}.`);
+    console.debug(`Running command \`${message.content}\` as <@${message.member.id}>.`);
 
     const permissions = ['user'];
     if (message.member.id === config.devID) if (client.op) permissions.push('trusted', 'staff', 'owner', 'dev'); else permissions.push('trusted', 'dev');
@@ -36,8 +36,8 @@ module.exports.run = async (bot, client, config, message, command, args) => {
     
     const com = client.commands.get(command);
     if (com) {
-      if (permissions.includes(com.help.permission)) return com.run(bot, client, config, message, command, args);
-      if (com.help.permission2 && permissions.includes(com.help.permission2)) return com.run(bot, client, config, message, command, args);
+      if (permissions.includes(com.help.permission)) return com.run(bot, client, config, message, newcommand, newargs);
+      if (com.help.permission2 && permissions.includes(com.help.permission2)) return com.run(bot, client, config, message, newcommand, newargs);
       return message.channel.send(embed.setDescription('You do not have the permission to run this command').addField('Required permission', `${com.help.permission}${com.help.permission2 ? `, ${com.help.permission2}` : ''}`).addField('Your permissions', permissions.join(', ')).setColor('RED'));
     }
   } catch (e) {
